2398594278599771a5ac0a34187260e319b23979
Tom <thomas.spencer@codurance.com>
2020-04-15 18:43:36 +0100

    Add process.env

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (request, response)=>{
