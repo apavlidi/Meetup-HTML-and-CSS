b17d4196c81a7bc648f3e965853d597c1cfef267
Alexis Pavlidis <apavlidi@it.teithe.gr>
2020-04-16 19:06:56 +0300

    obvious implementation on more info button

--- src/components/MoreInfo.js
+++ src/components/MoreInfo.js
@@ -1,5 +1,21 @@
 import React from "react";
+// const moreInfoButton = document.querySelector(".intro__more-info");
+// const showMoreInfo = () => {
+//     document.querySelector(".more-info").classList.remove('more-info--hidden')
+// }
+// moreInfoButton.addEventListener('click', showMoreInfo);
 
-const MoreInfo = () => <button>More info</button>;
 
-export { MoreInfo };
+function myFun() {
+    return "More info...";
+}
+
+const MoreInfo = () =>
+    <>
+        <button onClick={myFun}>More info</button>
+        <section className="more-info more-info--hidden">
+            <h2>More info...</h2>
+        </section>
+    </>
+
+export {MoreInfo};
--- src/components/MoreInfo.test.js
+++ src/components/MoreInfo.test.js
@@ -1,31 +1,39 @@
 import "@testing-library/jest-dom";
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import {render, fireEvent} from "@testing-library/react";
 
-import { MoreInfo } from "./MoreInfo.js";
+import {MoreInfo} from "./MoreInfo.js";
 
 describe("MoreInfo", () => {
-  let renderResult;
-  let button;
+    let renderResult;
+    let button;
 
-  beforeEach(() => {
-    renderResult = render(<MoreInfo />);
-
-    button = renderResult.getByText("More info");
-  });
-
-  it("has a button to show more info", () => {
-    expect(button).toBeInTheDocument();
-  });
-
-  describe("When the button is clicked", () => {
     beforeEach(() => {
-      fireEvent.click(button);
+        renderResult = render(<MoreInfo/>);
+
+        button = renderResult.getByText("More info");
     });
 
-    it("more info is show", () => {
-      const moreInfoSection = renderResult.queryByText("More info...");
-      expect(moreInfoSection).toBeInTheDocument();
+    it("has a button to show more info", () => {
+        expect(button).toBeInTheDocument();
     });
-  });
+
+    describe("Before the button is clicked", () => {
+        it("more info is not shown", () => {
+            const moreInfoSection = renderResult.queryByText("More info...");
+            expect(moreInfoSection).not.toBeInTheDocument();
+        })
+    })
+
+    describe("When the button is clicked", () => {
+        beforeEach(() => {
+            fireEvent.click(button);
+        });
+
+        it("more info is show", () => {
+            const moreInfoSection = renderResult.queryByText("More info...");
+            expect(moreInfoSection).toBeInTheDocument();
+        });
+
+    });
 });
